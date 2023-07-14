0ba5350f3d6b5af196d6979312813abb24bc0cdd
Nedzhat Kukharenko <60784933+Nedzhat@users.noreply.github.com>
2023-07-14 17:04:28 +0300

    Try deploy

---
+++ dist/assets/Projects-4e0ce939.js
@@ -0,0 +1,1 @@
+import{j as o,b as r,T as s}from"./index-4887a6c3.js";const e=()=>o.jsx(r,{w:"60%",bg:"bgColor",minH:"100vh",h:"auto",children:o.jsx(s,{children:"Projects page!"})});export{e as default};
---
+++ dist/assets/Workplace-ab4c003c.js
@@ -0,0 +1,1 @@
+import{j as o,b as r,T as e}from"./index-4887a6c3.js";const s=()=>o.jsx(r,{w:"60%",bg:"bgColor",minH:"100vh",h:"auto",children:o.jsx(e,{children:"Workplace page!"})});export{s as default};
