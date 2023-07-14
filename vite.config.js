fe8d625bc1a1d6f7cb4b011a03c881e91c783af4
Nedzhat Kukharenko <60784933+Nedzhat@users.noreply.github.com>
2023-07-14 16:48:25 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [react()],
+    base: "https://nedzhat.github.io/storage/",
     define: {
       "process.env": process.env,
     },
